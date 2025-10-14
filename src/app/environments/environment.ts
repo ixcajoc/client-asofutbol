c0c68cbe0e240c51d467011ae1191eeebf26ac18
Denis Ixcajoc <denisixcajoc2001@gmail.com>
2025-10-13 22:07:28 -0600

    production: cambiamos la url de la variable de entorno al proyecto de apis en vercel. Colocamos el parametro PRODUCTION en true

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,12 @@
+// export const environment = {
+//     production: false,
+//     url: 'http://localhost:3000/api/',
+
+// };
+
 export const environment = {
-    production: false,
-    url: 'http://localhost:3000/api/',
+    production: true,
+    url: 'https://server-asofutbol.vercel.app/api/',
 
 };
   
