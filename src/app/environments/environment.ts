c106d989a0c1353d366b564bc039ae35193f4c36
Denis Ixcajoc <denisixcajoc2001@gmail.com>
2025-11-05 22:16:18 -0600

    fix: quitamos seccion de proximos partidos.

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,9 +1,11 @@
+// local
 // export const environment = {
 //     production: false,
 //     url: 'http://localhost:3000/api/',
 
 // };
 
+// produccion
 export const environment = {
     production: true,
     url: 'https://server-asofutbol.vercel.app/api/',
